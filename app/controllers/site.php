a16752921a1cd39e717675b03de74f4ec9f333f0
Dan Parks <dan@eastofcleveland.com>
2010-02-23 01:18:36 -0500

    started mockups for data-entry screens

--- app/controllers/site.php
+++ app/controllers/site.php
@@ -3,7 +3,9 @@
 class Site extends Controller
 {
   function index() {
-    $this->load->view('pageTemplate', array('content' => $this->load->view('wireframes/dashboard', null, true)));
+    $this->load->view('pageTemplate', array('content' =>
+      $this->load->view('wireframes/dashboard', null, true) .
+      $this->load->view('wireframes/dataentry', null, true)));
   }
 }
 
--- app/views/wireframes/dashboard.php
+++ app/views/wireframes/dashboard.php
@@ -72,13 +72,13 @@
   </td>
   <td>
    <table class="report">
-    <tr><th>Month</th><th>Uniq. Visitors</th><th>Tot. Page Views</th><th>Tot. Visits</th><th>PV/Visit</th></tr>
-    <tr><td>09/2009</td><td>190,384</td><td>761,536</td><td>380,768</td><td>$38</td></tr>
-    <tr><td>10/2009</td><td>187,834</td><td>751,336</td><td>375,668</td><td>$38</td></tr>
-    <tr><td>11/2009</td><td>188,981</td><td>755,924</td><td>377,962</td><td>$38</td></tr>
-    <tr><td>12/2009</td><td>325,006</td><td>731,264</td><td>328,256</td><td>$33</td></tr>
-    <tr><td>01/2010</td><td>172,567</td><td>690,268</td><td>345,134</td><td>$35</td></tr>
-    <tr><td>02/2010</td><td>209,095</td><td>836,380</td><td>418,190</td><td>$42</td></tr>
+    <tr><th>Month</th><th>Uniq. Visitors</th><th>Tot. Page Views</th><th>Tot. Visits</th><th>Page Views/Visit</th></tr>
+    <tr><td>09/2009</td><td>190,384</td><td>761,536</td><td>380,768</td><td>2</td></tr>
+    <tr><td>10/2009</td><td>187,834</td><td>751,336</td><td>375,668</td><td>2</td></tr>
+    <tr><td>11/2009</td><td>188,981</td><td>755,924</td><td>377,962</td><td>2</td></tr>
+    <tr><td>12/2009</td><td>325,006</td><td>731,264</td><td>328,256</td><td>2.23</td></tr>
+    <tr><td>01/2010</td><td>172,567</td><td>690,268</td><td>345,134</td><td>2</td></tr>
+    <tr><td>02/2010</td><td>209,095</td><td>836,380</td><td>418,190</td><td>2</td></tr>
    </table>
   </td>
  </tr>
