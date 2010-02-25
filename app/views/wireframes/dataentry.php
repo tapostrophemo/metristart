d18be43a5fcc829969bfd33f1059ee8d3c6e7356
Dan Parks <dan@eastofcleveland.com>
2010-02-24 19:59:27 -0500

    yet more form/mockups

--- app/views/wireframes/dataentry.php
+++ app/views/wireframes/dataentry.php
@@ -1,5 +1,131 @@
 <hr/>
 
+<h2>Enter User Base Data</h2>
+
+<table class="entryForm rounded">
+ <tr>
+  <td><label>Month</label></td>
+  <td><input type="text" size="6"/></td>
+ </tr>
+ <tr>
+  <td><label>Registrations</label></td>
+  <td><input type="text" size="8"/></td>
+ </tr>
+ <tr>
+  <td><label>Activations</label></td>
+  <td><input type="text" size="8"/></td>
+ </tr>
+ <tr>
+  <td><label>Activation/Registration %</label></td>
+  <td>X% <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td><label>30-Day Retentions</label></td>
+  <td><input type="text" size="8"/></td>
+ </tr>
+ <tr>
+  <td><label>30-Day Retention/Activity %</label></td>
+  <td>X% <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td><label>90-Day Retentions</label></td>
+  <td><input type="text" size="8"/></td>
+ </tr>
+ <tr>
+  <td><label>90-Day Retention/Activity %</label></td>
+  <td>X% <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td><label>Paying Customers</label></td>
+  <td><input type="text" size="8"/></td>
+ </tr>
+ <tr>
+  <td><label>Num Paying / <br/>(Activations + 30-Day Retained)</label></td>
+  <td>X % <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td>
+   <input type="submit" value="Save"/>
+   <input type="button" value="Cancel"/>
+  </td>
+ </tr>
+</table>
+
+<hr/>
+
+<h2>Enter Revenues</h2>
+
+<table class="entryForm rounded">
+ <tr>
+  <td><label>Month</label></td>
+  <td><input type="text" size="6"/></td>
+ </tr>
+ <tr>
+  <td><label>Total Revenue</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Total Variable Costs</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Contribution Margin</label></td>
+  <td>$0 <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td><label>Total Fixed Costs</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Net Operating Income</label></td>
+  <td>$0 <small>(on-screen calculation)</small></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td>
+   <input type="submit" value="Save"/>
+   <input type="button" value="Cancel"/>
+  </td>
+ </tr>
+</table>
+
+<hr/>
+
+<h2>Enter Acquisition Costs</h2>
+
+<table class="entryForm rounded">
+ <tr>
+  <td><label>Month</label></td>
+  <td><input type="text" size="6"/></td>
+ </tr>
+ <tr>
+  <td><label>Paid Cost/Acquisition</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Net Cost/Acquisition</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Advertising Expenses</label></td>
+  <td><input type="text" size="10"/></td>
+ </tr>
+ <tr>
+  <td><label>Viral Ratio</label></td>
+  <td><input type="text" size="4"/></td>
+ </tr>
+ <tr>
+  <td></td>
+  <td>
+   <input type="submit" value="Save"/>
+   <input type="button" value="Cancel"/>
+  </td>
+ </tr>
+</table>
+
+<hr/>
+
 <h2>Enter Cash</h2>
 
 <table class="entryForm rounded">
@@ -100,6 +226,3 @@
  </tr>
 </table>
 
-<hr/>
-
-TODO: more data entry mockups
