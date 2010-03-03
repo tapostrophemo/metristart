643b909b1470aa2227418d1cc2e731e3f344f93e
Dan Parks <dan@eastofcleveland.com>
2010-03-03 01:22:12 -0500

    focus on first form field; small login form

--- app/views/pageTemplate.php
+++ app/views/pageTemplate.php
@@ -39,6 +39,16 @@
  </div>
 </div>
 
+<script type="text/javascript">
+window.onload = function () {
+  if (document.forms && document.forms[0] && document.forms[0].elements[0]) {
+    if (document.forms[0].elements[0].type == "text" || document.forms[0].elements[0].type == "password") {
+      document.forms[0].elements[0].focus();
+    }
+  }
+};
+</script>
+
 </body>
 </html>
 
--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -1,3 +1,15 @@
+<?php if (!$this->session->userdata('logged_in')): ?>
+<div style="text-align:right">
+ <?=form_open('login')?>
+ <small>
+  <label>Username</label> <input type="text" name="username" size="10"/>
+  <label>Password</label> <input type="password" name="password" size="10"/>
+  <input type="submit" value="Login"/>
+ </small>
+ </form>
+</div>
+<?php endif; ?>
+
 <p>MetriStart is a tool that will help you manage the "business" of your web-based startup. It
  shows you what metrics are truly important for this early-stage type of business, allowing you
  to focus on activities that provide the value you need to move your startup forward.</p>
