0ff3ba657baf29f091c95d690d0ce0b2678522fe
Dan Parks <dan@eastofcleveland.com>
2010-04-26 23:39:14 -0400

    cleanup

--- app/views/dataentry/userbase.php
+++ app/views/dataentry/userbase.php
@@ -21,32 +21,16 @@
   <td><input type="text" name="activations" size="8" value="<?=$editing ? $activations : set_value('activations')?>"/></td>
  </tr>
  <tr>
-  <td><label>Activation/Registration %</label></td>
-  <td>X% <small>(on-screen calculation)</small></td>
- </tr>
- <tr>
   <td><label>30-Day Retentions</label></td>
   <td><input type="text" name="retentions30" size="8" value="<?=$editing ? $retentions30 : set_value('retentions30')?>"/></td>
- </tr>
- <tr>
-  <td><label>30-Day Retention/Activity %</label></td>
-  <td>X% <small>(on-screen calculation)</small></td>
  </tr>
  <tr>
   <td><label>90-Day Retentions</label></td>
   <td><input type="text" name="retentions90" size="8" value="<?=$editing ? $retentions90 : set_value('retentions90')?>"/></td>
  </tr>
  <tr>
-  <td><label>90-Day Retention/Activity %</label></td>
-  <td>X% <small>(on-screen calculation)</small></td>
- </tr>
- <tr>
   <td><label>Paying Customers</label></td>
   <td><input type="text" name="paying" size="8" value="<?=$editing ? $payingCustomers : set_value('paying')?>"/></td>
- </tr>
- <tr>
-  <td><label>Num Paying / <br/>(Activations + 30-Day Retained)</label></td>
-  <td>X % <small>(on-screen calculation)</small></td>
  </tr>
  <tr>
   <td></td>
