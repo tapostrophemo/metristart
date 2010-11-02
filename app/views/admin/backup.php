9657476c9026ee02db9625dc6d8eae04466fa644
Dan Parks <dan@eastofcleveland.com>
2010-11-01 23:30:31 -0400

    admin menu for generating backup file

---
+++ app/views/admin/backup.php
@@ -0,0 +1,9 @@
+<h2>Admin: Backups</h2>
+
+<?=form_open('admin/backup')?>
+ <input type="hidden" name="junk" value="junk"/>
+ <input type="submit" value="Download" onclick="this.disabled=true"/>
+</form>
+
+<br/>
+
