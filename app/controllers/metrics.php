3b0bfeb2d4f669af9395a4fa44007480516a3e63
Dan Parks <dan@eastofcleveland.com>
2011-04-01 19:18:50 -0400

    refactoring; redirecting back to indivitual metric report after update/save

--- app/controllers/metrics.php
+++ app/controllers/metrics.php
@@ -2,103 +2,107 @@
 
 class Metrics extends MY_Controller
 {
+  var $userid;
+
   function __construct() {
     parent::__construct();
     $this->load->model('User');
     $this->load->model('Metric');
     log_message('debug', 'Metrics class initialized');
 
-    if (!$this->User->findById($this->session->userdata('userid'))) {
+    $this->userid = $this->session->userdata('userid');
+    if (!$this->User->findById($this->userid)) {
       $this->redirectWithError('Must be logged in.', '/login');
     }
   }
 
   function revenue($month = null, $year = null) {
-    $userid = $this->session->userdata('userid');
     $data['editing'] = $month != null && $year != null;
 
     if (!$this->form_validation->run('metrics_revenue')) {
       if ($data['editing']) {
-        $data = $this->Metric->getRevenue($userid, "$month/$year");
+        $data = $this->Metric->getRevenue($this->userid, "$month/$year");
         $data['editing'] = true;
       }
-      $data['last_entry_date'] = $this->Metric->lastEntryDate('revenue', $userid);
+      $data['last_entry_date'] = $this->Metric->lastEntryDate('revenue', $this->userid);
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/revenue.php', $data, true)));
     }
     else {
-      if ($this->input->post('editing')) {
-        $status = $this->Metric->updateRevenues($userid,
+      $editing = $this->input->post('editing');
+      $next = $this->_next($editing);
+
+      if ($editing) {
+        $status = $this->Metric->updateRevenues($this->userid,
           $this->input->post('segment'),
           $this->input->post('revenue'),
           $this->input->post('varcost'),
           $this->input->post('fixcost'));
       }
       else {
-        $status = $this->Metric->saveRevenues($userid,
+        $status = $this->Metric->saveRevenues($this->userid,
           $this->input->post('segment'),
           $this->input->post('revenue'),
           $this->input->post('varcost'),
           $this->input->post('fixcost'));
       }
       if ($status) {
-        $this->redirectWithMessage('Revenues saved.', '/dashboard');
+        $this->redirectWithMessage('Revenues saved.', $next);
       }
       else {
-        $this->redirectWithError('Problem saving revenues.', '/dashboard');
+        $this->redirectWithError('Problem saving revenues.', $next);
       }
     }
   }
 
   function revenues() {
-    $data = array('revenues' => $this->Metric->getRevenueReport($this->session->userdata('userid')));
+    $data = array('revenues' => $this->Metric->getRevenueReport($this->userid));
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/revenues', $data, true)));
   }
 
   function expense($month = null, $year = null) {
-    $userid = $this->session->userdata('userid');
-
     if (!$this->form_validation->run('metrics_expense')) {
-      $data = array('cash' => $this->Metric->getCash($userid));
+      $data = array('cash' => $this->Metric->getCash($this->userid));
       if (count($data['cash']) == 1) {
-        $data['burn'] = $this->Metric->getBurn($userid);
+        $data['burn'] = $this->Metric->getBurn($this->userid);
       }
       if ($data['editing'] = ($month != null && $year != null)) {
-        $data['expenses'] = $this->Metric->getExpense($userid, "$month/$year");
+        $data['expenses'] = $this->Metric->getExpense($this->userid, "$month/$year");
       }
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/expense.php', $data, true)));
     }
     else {
-      if ($this->input->post('editing')) {
-        $status = $this->Metric->updateExpense($userid,
+      $editing = $this->input->post('editing');
+      $next = $this->_next($editing);
+
+      if ($editing) {
+        $status = $this->Metric->updateExpense($this->userid,
           $this->input->post('segment'),
           $this->input->post('expenses'),
           $this->input->post('description'));
       }
       else {
-        $status = $this->Metric->saveExpense($userid,
+        $status = $this->Metric->saveExpense($this->userid,
           $this->input->post('segment'),
           $this->input->post('expenses'),
           $this->input->post('description'));
       }
 
       if ($status) {
-        $this->redirectWithMessage('Expenses saved.', '/dashboard');
+        $this->redirectWithMessage('Expenses saved.', $next);
       }
       else {
-        $this->redirectWithError('Problem saving expenses.', '/dashboard');
+        $this->redirectWithError('Problem saving expenses.', $next);
       }
     }
   }
 
   function infusion() {
-    $userid = $this->session->userdata('userid');
-
     if (!$this->form_validation->run('metrics_infusion')) {
-      $data = array('cash' => $this->Metric->getCash($userid));
+      $data = array('cash' => $this->Metric->getCash($this->userid));
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/expense.php', $data, true)));
     }
     else {
-      $status = $this->Metric->saveCash($userid, $this->input->post('amount'));
+      $status = $this->Metric->saveCash($this->userid, $this->input->post('amount'));
       if ($status) {
         $this->redirectWithMessage('Initial cash infusion saved.', '/dashboard');
       }
@@ -109,27 +113,26 @@ class Metrics extends MY_Controller
   }
 
   function burnrate() {
-    $data = array('expenses' => $this->Metric->getBurnReport($this->session->userdata('userid')));
+    $data = array('expenses' => $this->Metric->getBurnReport($this->userid));
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/burnrate', $data, true)));
   }
 
   function userbase($month = null, $year = null) {
-    $userid = $this->session->userdata('userid');
     $data['editing'] = $month != null && $year != null;
     if (!$this->form_validation->run('metrics_userbase')) {
       if ($data['editing']) {
-        $data = $this->Metric->getUserbase($userid, "$month/$year");
+        $data = $this->Metric->getUserbase($this->userid, "$month/$year");
         $data['editing'] = true;
       }
-      $data['last_entry_date'] = $this->Metric->lastEntryDate('registrations', $userid);
+      $data['last_entry_date'] = $this->Metric->lastEntryDate('registrations', $this->userid);
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/userbase', $data, true)));
     }
     else {
-      $userid = $this->session->userdata('userid');
-      $month = $this->input->post('segment');
+      $editing = $this->input->post('editing');
+      $next = $this->_next($editing);
 
-      if ($this->input->post('editing')) {
-        $status = $this->Metric->updateUserbase($this->session->userdata('userid'),
+      if ($editing) {
+        $status = $this->Metric->updateUserbase($this->userid,
           $this->input->post('segment'),
           $this->input->post('registrations'),
           $this->input->post('activations'),
@@ -138,7 +141,7 @@ class Metrics extends MY_Controller
           $this->input->post('paying'));
       }
       else {
-        $status = $this->Metric->saveUserbase($this->session->userdata('userid'),
+        $status = $this->Metric->saveUserbase($this->userid,
           $this->input->post('segment'),
           $this->input->post('registrations'),
           $this->input->post('activations'),
@@ -147,41 +150,42 @@ class Metrics extends MY_Controller
           $this->input->post('paying'));
       }
       if ($status) {
-        $this->redirectWithMessage('Userbase data saved.', '/dashboard');
+        $this->redirectWithMessage('Userbase data saved.', $next);
       }
       else {
-        $this->redirectWithError('Problem saving userbase data.', '/dashboard');
+        $this->redirectWithError('Problem saving userbase data.', $next);
       }
     }
   }
 
   function ub() {
-    $data = array('userbase' => $this->Metric->getUserbaseReport($this->session->userdata('userid')));
+    $data = array('userbase' => $this->Metric->getUserbaseReport($this->userid));
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/userbase', $data, true)));
   }
 
   function web($month = null, $year = null) {
-    $userid = $this->session->userdata('userid');
-
     if (!$this->form_validation->run('metrics_web')) {
       $data['editing'] = false;
       if ($month != null && $year != null) {
-        $data = $this->Metric->getWeb($userid, "$month/$year");
+        $data = $this->Metric->getWeb($this->userid, "$month/$year");
         $data['editing'] = true;
       }
-      $data['last_entry_date'] = $this->Metric->lastEntryDate('pageViews', $userid);
+      $data['last_entry_date'] = $this->Metric->lastEntryDate('pageViews', $this->userid);
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/web', $data, true)));
     }
     else {
-      if ($this->input->post('editing')) {
-        $status = $this->Metric->updateWeb($userid,
+      $editing = $this->input->post('editing');
+      $next = $this->_next($editing);
+
+      if ($editing) {
+        $status = $this->Metric->updateWeb($this->userid,
           $this->input->post('segment'),
           $this->input->post('uniques'),
           $this->input->post('views'),
           $this->input->post('visits'));
       }
       else {
-        $status = $this->Metric->saveWeb($userid,
+        $status = $this->Metric->saveWeb($this->userid,
           $this->input->post('segment'),
           $this->input->post('uniques'),
           $this->input->post('views'),
@@ -189,34 +193,35 @@ class Metrics extends MY_Controller
       }
 
       if ($status) {
-        $this->redirectWithMessage('Web metrics saved.', '/dashboard');
+        $this->redirectWithMessage('Web metrics saved.', $next);
       }
       else {
-        $this->redirectWithError('Problem saving web metrics.', '/dashboard');
+        $this->redirectWithError('Problem saving web metrics.', $next);
       }
     }
   }
 
   function wb() {
-    $data = array('web' => $this->Metric->getWebMetricsReport($this->session->userdata('userid')));
+    $data = array('web' => $this->Metric->getWebMetricsReport($this->userid));
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/web', $data, true)));
   }
 
   function acquisition($month = null, $year = null) {
-    $userid = $this->session->userdata('userid');
-
     if (!$this->form_validation->run('metrics_acquisition')) {
       $data['editing'] = false;
       if (!$month == null && !$year == null) {
-        $data = $this->Metric->getAcquisitions($userid, "$month/$year");
+        $data = $this->Metric->getAcquisitions($this->userid, "$month/$year");
         $data['editing'] = true;
       }
-      $data['last_entry_date'] = $this->Metric->lastEntryDate('acqPaidCost', $userid);
+      $data['last_entry_date'] = $this->Metric->lastEntryDate('acqPaidCost', $this->userid);
       $this->load->view('pageTemplate', array('content' => $this->load->view('dataentry/acquisition', $data, true)));
     }
     else {
-      if ($this->input->post('editing')) {
-        $status = $this->Metric->updateAcquisitions($userid,
+      $editing = $this->input->post('editing');
+      $next = $this->_next($editing);
+
+      if ($editing) {
+        $status = $this->Metric->updateAcquisitions($this->userid,
           $this->input->post('segment'),
           $this->input->post('acqPaidCost'),
           $this->input->post('acqNetCost'),
@@ -224,7 +229,7 @@ class Metrics extends MY_Controller
           $this->input->post('viratio'));
       }
       else {
-        $status = $this->Metric->saveAcquisitions($userid,
+        $status = $this->Metric->saveAcquisitions($this->userid,
           $this->input->post('segment'),
           $this->input->post('acqPaidCost'),
           $this->input->post('acqNetCost'),
@@ -233,27 +238,37 @@ class Metrics extends MY_Controller
       }
 
       if ($status) {
-        $this->redirectWithMessage('Acquisition costs saved.', '/dashboard');
+        $this->redirectWithMessage('Acquisition costs saved.', $next);
       }
       else {
-        $this->redirectWithError('Problems saving acquisition costs.', '/dashboard');
+        $this->redirectWithError('Problems saving acquisition costs.', $next);
       }
     }
   }
 
   function acq() {
-    $data = array('acquisition' => $this->Metric->getAcquisitionCostsReport($this->session->userdata('userid')));
+    $data = array('acquisition' => $this->Metric->getAcquisitionCostsReport($this->userid));
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/acquisition', $data, true)));
   }
 
   function all() {
-    $userid = $this->session->userdata('userid');
-    $data['revenues'] = $this->Metric->getRevenueReport($userid);
-    $data['expenses'] = $this->Metric->getBurnReport($userid);
-    $data['userbase'] = $this->Metric->getUserbaseReport($userid);
-    $data['web'] = $this->Metric->getWebMetricsReport($userid);
-    $data['acquisition'] = $this->Metric->getAcquisitionCostsReport($userid);
+    $data['revenues'] = $this->Metric->getRevenueReport($this->userid);
+    $data['expenses'] = $this->Metric->getBurnReport($this->userid);
+    $data['userbase'] = $this->Metric->getUserbaseReport($this->userid);
+    $data['web'] = $this->Metric->getWebMetricsReport($this->userid);
+    $data['acquisition'] = $this->Metric->getAcquisitionCostsReport($this->userid);
     $this->load->view('pageTemplate', array('content' => $this->load->view('metrics/fullReport', $data, true)));
+  }
+
+  function _next($editing = false) {
+    $referrer = $this->input->server('HTTP_REFERER');
+    if (!$editing) return '/dashboard';
+    if (strpos($referrer, '/acquisition/') !== false) return '/metrics/acq';
+    if (strpos($referrer, '/expense/')     !== false) return '/metrics/burnrate';
+    if (strpos($referrer, '/revenue/')     !== false) return '/metrics/revenues';
+    if (strpos($referrer, '/userbase/')    !== false) return '/metrics/ub';
+    if (strpos($referrer, '/web/')         !== false) return '/metrics/wb';
+    return '/dashboard';
   }
 }
 
